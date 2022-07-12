18d69e9697fbc0279d843ebc2957234ae91f9287
hdangnguyen <hdangprod@gmail.com>
2022-07-12 08:27:41 +0700

    second commit

---
+++ src/components/DetailComponent.js
@@ -0,0 +1,26 @@
+const {
+  Card,
+  CardImg,
+  CardBody,
+  CardSubtitle,
+  CardTitle,
+  CardText,
+} = require("reactstrap");
+
+const DetailComponent = ({ product }) => {
+  const { name, image, category, price } = product;
+  return (
+    <Card className="w-25">
+      <CardImg src={image} alt={name} className="" />
+      <CardBody>
+        <CardSubtitle className="text-muted" tag="h6">
+          {category}
+        </CardSubtitle>
+        <CardTitle>{name}</CardTitle>
+        <CardText>{price}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default DetailComponent;
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import HeaderComponent from "../components/HeaderComponent";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  );
+};
+export default HomePage;
